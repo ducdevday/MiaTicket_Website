ae26a82d07b4c7a8ac4f741be57fcc984b9be4e9
ducdevday <ducdevday@gmail.com>
2024-11-12 15:31:57 +0700

    Update Register UI

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -23,16 +23,51 @@ import { CreateAccountRequest } from '../../dto/request/create-account-request';
 import { Router } from '@angular/router';
 import { EMAIL_VERIFY_PATH, LOGIN_PATH } from '../../app.routes';
 import { VerifyType } from '../../dto/enum/verify-type';
+import { DropdownModule } from 'primeng/dropdown';
+import { Gender } from '../../dto/enum/gender';
+import { Role } from '../../dto/enum/role';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, CalendarModule, ToastModule, HttpClientModule],
+  imports: [
+    ReactiveFormsModule,
+    CalendarModule,
+    ToastModule,
+    HttpClientModule,
+    DropdownModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   providers: [ToastService, MessageService, AccountService],
 })
 export class RegisterComponent {
+  genders = [
+    {
+      name: 'Male',
+      value: Gender.Male,
+    },
+    {
+      name: 'Female',
+      value: Gender.Female,
+    },
+    {
+      name: 'Other',
+      value: Gender.Other,
+    },
+  ];
+
+  roles = [
+    {
+      name: 'Customer',
+      value: Role.Customer,
+    },
+    {
+      name: 'Organizer',
+      value: Role.Organizer,
+    },
+  ];
+
   registerForm!: FormGroup;
   constructor(
     private fb: FormBuilder,
@@ -47,12 +82,17 @@ export class RegisterComponent {
         '',
         [Validators.required, Validators.pattern(PASSWORD_PATTERN)],
       ],
+      confirmPassword: [
+        '',
+        [Validators.required, Validators.pattern(PASSWORD_PATTERN)],
+      ],
       phoneNumber: [
         '',
         [Validators.required, Validators.pattern(PHONE_NUMBER_PATTERN)],
       ],
       birthDate: ['', [Validators.required, this.pastDateValidator]],
       gender: [null, Validators.required],
+      role: [null, Validators.required],
     });
   }
 
@@ -67,39 +107,31 @@ export class RegisterComponent {
   }
 
   onSubmit() {
-    var isNameValid = this.registerForm.controls['name'].valid;
-    var isEmailValid = this.registerForm.controls['email'].valid;
-    var isPasswordValid = this.registerForm.controls['password'].valid;
-    var isPhoneNumberValid = this.registerForm.controls['phoneNumber'].valid;
-    var isBirthDateValid = this.registerForm.controls['birthDate'].valid;
-    var isGenderValid = this.registerForm.controls['gender'].valid;
-    if (
-      isNameValid &&
-      isEmailValid &&
-      isPasswordValid &&
-      isPhoneNumberValid &&
-      isBirthDateValid &&
-      isGenderValid
-    ) {
-      var name = this.registerForm.controls['name'].value;
-      var email = this.registerForm.controls['email'].value;
-      var password = this.registerForm.controls['password'].value;
-      var phoneNumber = this.registerForm.controls['phoneNumber'].value;
-      var birthDate = TimeUtil.formatToISOString(
-        this.registerForm.controls['birthDate'].value
-      );
-      var gender = this.registerForm.controls['gender'].value;
+    const isRegisterFormValid = this.registerForm.valid;
+    if (isRegisterFormValid) {
+      const {
+        name,
+        email,
+        password,
+        confirmPassword,
+        phoneNumber,
+        birthDate,
+        gender,
+        role,
+      } = this.registerForm.value;
+
       var createAccountRequest = new CreateAccountRequest(
         name,
         email,
         password,
+        confirmPassword,
         phoneNumber,
         birthDate,
-        gender
+        gender.value,
+        role.value
       );
       this.accountService.createAccount(createAccountRequest).subscribe({
         next: (response) => {
-          console.log(response);
           this.toastService.showSuccess('Register Successfully');
           this.router.navigate([EMAIL_VERIFY_PATH], {
             queryParams: {
--- src/app/dto/request/create-account-request.ts
+++ src/app/dto/request/create-account-request.ts
@@ -1,23 +1,32 @@
+import { Gender } from '../enum/gender';
+import { Role } from '../enum/role';
+
 export class CreateAccountRequest {
   name!: string;
   email!: string;
   password!: string;
+  confirmPassword!: string;
   phoneNumber!: string;
   birthDate!: string;
-  gender!: number;
+  gender!: Gender;
+  role!: Role;
   constructor(
     name: string,
     email: string,
     password: string,
+    confirmPassword: string,
     phoneNumber: string,
     birthDate: string,
-    gender: number
+    gender: Gender,
+    role: Role
   ) {
     this.name = name;
     this.email = email;
     this.password = password;
+    this.confirmPassword = confirmPassword;
     this.phoneNumber = phoneNumber;
     this.birthDate = birthDate;
     this.gender = gender;
+    this.role = role;
   }
 }
